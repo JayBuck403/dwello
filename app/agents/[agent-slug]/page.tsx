695f8c742eb6801a5e10b651a928261b8953b894
JayBuck403 <jaybuck403@gmail.com>
2025-07-22 00:31:16 +0000

    Added more agent privacy

--- app/agents/[agent-slug]/page.tsx
+++ app/agents/[agent-slug]/page.tsx
@@ -10,6 +10,9 @@ import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { formatCurrency } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
 
 interface Properties {
   id: string;
@@ -40,6 +43,15 @@ export default function AgentProfilePage() {
   const agentSlug = params?.["agent-slug"] as string;
   const [agent, setAgent] = useState<Agent | null>(null);
   const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState<FirebaseUser | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
 
   useEffect(() => {
     async function fetchAgent() {
@@ -109,41 +121,53 @@ export default function AgentProfilePage() {
                 </p>
                 <div className="space-y-2">
                   <div className="flex flex-col gap-2 mt-4">
-                    <Button
-                      asChild
-                      className="flex items-center justify-center gap-2 text-white hover:bg-green-600"
-                    >
-                      <a
-                        href={`https://wa.me/${agent.phone_whatsapp}?text=Hello, I'd like to inquire about your listings.`} // Check if phone_whatsapp is in international format
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-full h-full flex items-center justify-center gap-2"
-                      >
-                        <svg
-                          viewBox="0 0 32 32"
-                          fill="white"
-                          className="h-5 w-5"
+                    {currentUser ? (
+                      <>
+                        <Button
+                          asChild
+                          className="flex items-center justify-center gap-2 text-white hover:bg-green-600"
                         >
-                          <path
-                            d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z"
-                            fillRule="evenodd"
-                          ></path>
-                        </svg>
-                        WhatsApp
-                      </a>
-                    </Button>
-                    <Button
-                      asChild
-                      variant="outline"
-                      className="flex items-center justify-center gap-2 text-primary border-primary hover:bg-primary/10"
-                    >
-                      <a
-                        href={`tel:${agent.phone_call}`}
-                        className="w-full h-full flex items-center justify-center gap-2"
+                          <a
+                            href={`https://wa.me/${agent.phone_whatsapp}?text=Hello, I'd like to inquire about your listings.`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="w-full h-full flex items-center justify-center gap-2"
+                          >
+                            <svg
+                              viewBox="0 0 32 32"
+                              fill="white"
+                              className="h-5 w-5"
+                            >
+                              <path
+                                d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z"
+                                fillRule="evenodd"
+                              ></path>
+                            </svg>
+                            WhatsApp
+                          </a>
+                        </Button>
+                        <Button
+                          asChild
+                          variant="outline"
+                          className="flex items-center justify-center gap-2 text-primary border-primary hover:bg-primary/10"
+                        >
+                          <a
+                            href={`tel:${agent.phone_call}`}
+                            className="w-full h-full flex items-center justify-center gap-2"
+                          >
+                            <Phone className="h-4 w-4" /> Call
+                          </a>
+                        </Button>
+                      </>
+                    ) : (
+                      <Button
+                        variant="outline"
+                        className="flex items-center justify-center gap-2 text-primary border-primary hover:bg-primary/10"
+                        onClick={() => router.push("/login")}
                       >
-                        <Phone className="h-4 w-4" /> Call
-                      </a>
-                    </Button>
+                        <Phone className="h-4 w-4" /> Login to contact agent
+                      </Button>
+                    )}
                   </div>
                   <p className="text-gray-700 text-sm flex items-center justify-center gap-2">
                     <Mail className="h-4 w-4 text-primary" /> {agent.email}
